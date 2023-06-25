73f47bc1795fa7717ed74e11254e3a2a826514f7
Supanut1911 <ynotmela@gmail.com>
2023-06-25 16:46:14 +0700

    🚀 - apply terraform to launch ec2 instance

--- instance-ec2.tf
+++ instance-ec2.tf
@@ -1,5 +1,18 @@
-provider "AWS" {
+provider "aws" {
   region = "ap-southeast-1"
-  access_key = ""
-  secret_key = ""
+  # access_key = ""
+  # secret_key = ""
 }
+
+resource "aws_instance" "myintroec2" {
+  ami                    = "ami-0df7a207adb9748c7"
+  instance_type          = "t2.micro"
+  availability_zone      = "ap-southeast-1a"
+  key_name               = "terraform-dev-keypair"
+  vpc_security_group_ids = ["*******"]
+  tags = {
+    Name    = "Terraform-EC2-instance"
+    Project = "PoC-IaC-Terraform"
+  }
+}
+
